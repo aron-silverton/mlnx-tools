aron-silverton/mlnx-tools

commit dce648844c6bb9e25b2e6edd9878de8ac2f74e94

roce_config: Adjust configuration steps for VFs
Identify Virtual Functions using Cache Line Size and skip mlnx_qos &
mstconfig calls for the respective devices. Also, bump up the minor
version to 5.

Orabug: 27482819

Signed-off-by: Avinash Repaka <avinash.repaka@oracle.com>
Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
Signed-off-by: Aron Silverton <aron.silverton@oracle.com>

buildrpm/oracle-rdma-tools.spec
@@ -75,6 +75,9 @@ rm -rf %{buildroot}
 %{_bindir}/*
 
 %changelog
+* Wed Sep 26 2018 Aron Silverton <aron.silverton@oracle.com> - 0:0.6.0
+- roce_config: Adjust configuration steps for VFs (Avinash Repaka) [Orabug: 27482819]
+
 * Mon Aug 27 2018 Aron Silverton <aron.silverton@oracle.com> - 0:0.6.0-2
 - Add "vos" to RPM release number (Aron Silverton) [Orabug 28550856]
 

oracle/roce_config.sh
@@ -48,7 +48,7 @@ TOS_MAP=32,32,64,96,128,160,192,192
 PFC_CONFIG=""
 DEFAULT_PFC_CONFIG=0,1,1,1,1,1,1,0
 MAJOR_VERSION=1
-MINOR_VERSION=4
+MINOR_VERSION=5
 
 echo ""
 
@@ -260,6 +260,19 @@ fi
 PORT="$(ibdev2netdev | grep $NETDEV | head -1 | cut -f 3 -d " ")"
 echo "NETDEV=$NETDEV; IBDEV=$IBDEV; PORT=$PORT"
 
+PCI_ADDR="$(grep PCI_SLOT_NAME /sys/class/net/$NETDEV/device/uevent | cut -d "=" -f 2)"
+if [ -z "$PCI_ADDR" ] ; then
+	>&2 echo " - Failed to obtain PCI ADDRESS for netdev \"$NETDEV\""
+	exit 1
+fi
+
+#We use cache line size to identify virtual functions. It is 00 for VFs.
+CACHE_LINE_SIZE="$(setpci -s $PCI_ADDR c.b)"
+if [[ $? != 0 ]] ; then
+	>&2 echo " - Failed to obtain Cache Line Size"
+	exit 1
+fi
+
 if [[ $TRUST_MODE != "dscp" && $TRUST_MODE != "pcp" ]] ; then
 	>&2 echo " - Option -t can take only dscp or pcp as input"
 	exit 1
@@ -304,17 +317,6 @@ if [ ! -d "/sys/kernel/config/rdma_cm/$IBDEV" ] ; then
 	fi
 fi
 
-set_rocev2_default
-set_tos_mapping
-set_default_tos
-config_trust_mode
-config_pfc
-
-PCI_ADDR="$(ethtool -i $NETDEV | grep "bus-info" | cut -f 2 -d " ")"
-if [ -z "$PCI_ADDR" ] ; then
-	>&2 echo " - Failed to obtain PCI ADDRESS for netdev \"$NETDEV\""
-	exit 1
-fi
 if (! cat /proc/mounts |grep /sys/kernel/debug > /dev/null) ; then
 	mount -t debugfs none /sys/kernel/debug
 	if [[ $? != 0 ]] ; then
@@ -322,7 +324,15 @@ if (! cat /proc/mounts |grep /sys/kernel/debug > /dev/null) ; then
 		exit 1
 	fi
 fi
-enable_congestion_control
+
+set_rocev2_default
+set_tos_mapping
+set_default_tos
+if [[ $CACHE_LINE_SIZE != "00" ]] ; then
+	config_trust_mode
+	config_pfc
+	enable_congestion_control
+fi
 set_cnp_priority
 
 echo ""

commit 07c9ef51cac5736d4f6de848139a9b87a528494c

roce_config: Do configuration based on PF or VF
Some configuration steps are only relevant to PFs and should not be
performed for VFs. Check for the PCI Express SR-IOV Extended Capability
to determine if the device is a PF.

Fixes: dce648844c6b ("roce_config: Adjust configuration steps for VFs")

Orabug: 27482819

Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
Reviewed-by: Gerald Gibson <gerald.gibson@oracle.com>

buildrpm/oracle-rdma-tools.spec
@@ -74,6 +74,9 @@ rm -rf %{buildroot}
 %{_bindir}/*
 
 %changelog
+* Tue Dec 04 2018 Aron Silverton <aron.silverton@oracle.com> - 0:0.7.0
+- roce_config: Do configuration based on PF or VF (Aron Silverton) [Orabug: 27482819]
+
 * Fri Nov 09 2018 Aron Silverton <aron.silverton@oracle.com> - 0:0.7.0-2
 - spec: Change "vos" to "ora" and update summaries and descriptions [Orabug: 29128747]
 

oracle/roce_config.sh
@@ -266,11 +266,14 @@ if [ -z "$PCI_ADDR" ] ; then
 	exit 1
 fi
 
-#We use cache line size to identify virtual functions. It is 00 for VFs.
-CACHE_LINE_SIZE="$(setpci -s $PCI_ADDR c.b)"
+# Check for the SR-IOV extended capability to identify VFs and PFs.
+# A PF will return a value of '10' and a VF will return NULL.
+#
+# setpci will return 0 if the register exists and 1 if it does not
+# so we should not exit just because we get a non-zero return value
+ECAP_SRIOV="$(setpci -s "$PCI_ADDR" ECAP_SRIOV.b 2> /dev/null)"
 if [[ $? != 0 ]] ; then
-	>&2 echo " - Failed to obtain Cache Line Size"
-	exit 1
+	unset ECAP_SRIOV
 fi
 
 if [[ $TRUST_MODE != "dscp" && $TRUST_MODE != "pcp" ]] ; then
@@ -328,14 +331,12 @@ fi
 set_rocev2_default
 set_tos_mapping
 set_default_tos
-if [[ $CACHE_LINE_SIZE != "00" ]] ; then
+
+# Only do the following for PFs (ECAP_SRIOV is not NULL)
+if [[ -n $ECAP_SRIOV ]] ; then
 	config_trust_mode
 	config_pfc
 	enable_congestion_control
-	# set_cnp_priority() does not work in secure boot environments. This step
-	# is redundant because these settings are made in the firmware images for
-	# Exadata so we simply do not execute this function.
-	#set_cnp_priority
 fi
 
 echo ""